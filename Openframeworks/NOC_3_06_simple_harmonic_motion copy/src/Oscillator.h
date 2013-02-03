28d00b7016f11b42848f2050673aee5bc3de5c74
Jason McDermott <jason@jasonmcdermott.net>
2013-02-03 16:31:53 +1100

    new examples from chapter 3

---
+++ Openframeworks/NOC_3_06_simple_harmonic_motion copy/src/Oscillator.h
@@ -0,0 +1,14 @@
+//
+//  Oscillator.h
+//  NOC_3_07_oscillating_objects
+//
+//  Created by Jason McDermott on 3/02/13.
+//
+//
+
+#ifndef NOC_3_07_oscillating_objects_Oscillator_h
+#define NOC_3_07_oscillating_objects_Oscillator_h
+
+
+
+#endif
